81a7c04ef4a73a9ee33dc6ecec8c54ab1519eafe
willvernon <wmvernon@proton.me>
2024-03-03 13:23:06 -0500

    math algo v1

--- nba-sq1.py
+++ nba-sq1.py
@@ -6,98 +6,92 @@
 def alg(last_game):
     points = 0
 
-    if last_game == "t1wa":
+    if last_game == "3wa":
         points += 7
-    elif last_game == "t1wh":
+    elif last_game == "3wh":
         points += 5
-    elif last_game == "t2wa":
+    elif last_game == "2wa":
         points += 4
-    elif last_game == "t2wh":
+    elif last_game == "2wh":
         points += 3
-    elif last_game == "t3wa":
+    elif last_game == "1wa":
         points += 1
-    elif last_game == "t3wh":
+    elif last_game == "1wh":
         points += 1
 
     return points
 
-def tier_points(team_tier):
-    points = 0
-
-    if team_tier == "t1":
-        points += 5
-    elif team_tier == "t2":
-        points += 3
-    elif team_tier == "t3":
-        points += 1
-    
-    return points
 
 def main():
     team1_ttl = 0
     team2_ttl = 0
+    n = 1
 
     team1_name = input("Enter Team 1: ")
-    team_tier = input("Enter Team Tier (t1,t2,t3): ")
-    team1_ttl += tier_points(team_tier)
-    current_gm_home = input("Home Team in Comparison (y/n): ")
-    if current_gm_home == 'y':
+    tm_lvl = int(input("Enter Team Level (1,2,3): "))
+    team1_ttl += (tm_lvl**1.5)
+    current_gm_home = input("Home Team in Comparison (binary): ")
+    if current_gm_home == '1':
         team1_ttl += 2
     else:
         team2_ttl += 2
-    tm1_last_game1 = input("Enter Last Game 1 (t1wh, t1la, etc.): ")
-    team1_ttl += alg(tm1_last_game1)
-    tm1_last_game2 = input("Enter Last Game 2 (t1wh, t1la, etc.): ")
-    team1_ttl += alg(tm1_last_game2)
-    tm1_last_game3 = input("Enter Last Game 3 (t1wh, t1la, etc.): ")
-    team1_ttl += alg(tm1_last_game3)
-    tm1_last_game4 = input("Enter Last Game 4 (t1wh, t1la, etc.): ")
-    team1_ttl += alg(tm1_last_game4)
+    while n < 5:
+        w = int(input("Is Game "+str(n)+" a Win (binary): "))
+        if w == 0:
+            n+=1
+            continue
+        l = int(input("What was the Opp Tm Lvl (worst 1,2,3 best): "))
+        if l == 1:
+            n+=1
+            continue
+        a = int(input("Is Game "+str(n)+" an away game (binary): "))
+        team1_ttl += (w*(l**1.5 + 2*a))
+        n += 1
 
-    team2_name = input("Enter Team 2: ")
-    team_tier2 = input("Enter Team2 Tier (t1,t2,t3): ")
-    team2_ttl += tier_points(team_tier2)
-    tm2_last_game1 = input("Enter Last Game 1 (t1wh, t1la, etc.): ")
-    team2_ttl += alg(tm2_last_game1)
-    tm2_last_game2 = input("Enter Last Game 2 (t1wh, t1la, etc.): ")
-    team2_ttl += alg(tm2_last_game2)
-    tm2_last_game3 = input("Enter Last Game 3 (t1wh, t1la, etc.): ")
-    team2_ttl += alg(tm2_last_game3)
-    tm2_last_game4 = input("Enter Last Game 4 (t1wh, t1la, etc.): ")
-    team2_ttl += alg(tm2_last_game4)
-
-    pg_comp = input("Who Has Better PG (team1, team2): ")
-    if pg_comp == "team1":
-        team1_ttl += 1 
-    else:
-        team2_ttl += 1 
-   
-    sg_comp = input("Who Has Better SG (team1, team2): ")
-    if sg_comp == "team1":
-        team1_ttl += 1 
-    else:
-        team2_ttl += 1 
-   
-    sf_comp = input("Who Has Better SF (team1, team2): ")
-    if sf_comp == "team1":
-        team1_ttl += 1 
-    else:
-        team2_ttl += 1 
-    
-    pf_comp = input("Who Has Better PF (team1, team2): ")
-    if pf_comp == "team1":
-        team1_ttl += 1 
-    else:
-        team2_ttl += 1 
-
-    c_comp = input("Who Has Better C (team1, team2): ")
-    if c_comp == "team1":
-        team1_ttl += 1 
-    else:
-        team2_ttl += 1 
+    # team2_name = input("Enter Team 2: ")
+    # team_tier2 = input("Enter Team2 Tier (t1,t2,t3): ")
+    # team2_ttl += tier_points(team_tier2)
+    # tm2_last_game1 = input("Enter Last Game 1 (t1wh, t1la, etc.): ")
+    # team2_ttl += alg(tm2_last_game1)
+    # tm2_last_game2 = input("Enter Last Game 2 (t1wh, t1la, etc.): ")
+    # team2_ttl += alg(tm2_last_game2)
+    # tm2_last_game3 = input("Enter Last Game 3 (t1wh, t1la, etc.): ")
+    # team2_ttl += alg(tm2_last_game3)
+    # tm2_last_game4 = input("Enter Last Game 4 (t1wh, t1la, etc.): ")
+    # team2_ttl += alg(tm2_last_game4)
+    #
+    # pg_comp = input("Who Has Better PG (team1, team2): ")
+    # if pg_comp == "team1":
+    #     team1_ttl += 1 
+    # else:
+    #     team2_ttl += 1 
+    #
+    # sg_comp = input("Who Has Better SG (team1, team2): ")
+    # if sg_comp == "team1":
+    #     team1_ttl += 1 
+    # else:
+    #     team2_ttl += 1 
+    #
+    # sf_comp = input("Who Has Better SF (team1, team2): ")
+    # if sf_comp == "team1":
+    #     team1_ttl += 1 
+    # else:
+    #     team2_ttl += 1 
+    #
+    # pf_comp = input("Who Has Better PF (team1, team2): ")
+    # if pf_comp == "team1":
+    #     team1_ttl += 1 
+    # else:
+    #     team2_ttl += 1 
+    #
+    # c_comp = input("Who Has Better C (team1, team2): ")
+    # if c_comp == "team1":
+    #     team1_ttl += 1 
+    # else:
+    #     team2_ttl += 1 
 
     print("Team1: " + team1_name + " " + str(team1_ttl) + " pts")
-    print("Team2: " + team2_name + " " + str(team2_ttl) + " pts")
+    # print("Team2: " + team2_name + " " + str(team2_ttl) + " pts")
 
 if __name__ == "__main__":
     main()
