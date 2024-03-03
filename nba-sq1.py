5e4d1c26db9871cfc362236a2292f86737b06fb5
willvernon <wmvernon@proton.me>
2024-03-03 06:09:47 -0500

    sequence v2

--- nba-sq1.py
+++ nba-sq1.py
@@ -42,9 +42,9 @@ def main():
     team1_ttl += tier_points(team_tier)
     current_gm_home = input("Home Team in Comparison (y/n): ")
     if current_gm_home == 'y':
-        team1_ttl += 3
+        team1_ttl += 2
     else:
-        team2_ttl += 3
+        team2_ttl += 2
     tm1_last_game1 = input("Enter Last Game 1 (t1wh, t1la, etc.): ")
     team1_ttl += alg(tm1_last_game1)
     tm1_last_game2 = input("Enter Last Game 2 (t1wh, t1la, etc.): ")
@@ -65,6 +65,36 @@ def main():
     team2_ttl += alg(tm2_last_game3)
     tm2_last_game4 = input("Enter Last Game 4 (t1wh, t1la, etc.): ")
     team2_ttl += alg(tm2_last_game4)
+
+    pg_comp = input("Who Has Better PG (team1, team2): ")
+    if pg_comp == "team1":
+        team1_ttl += 1 
+    else:
+        team2_ttl += 1 
+   
+    sg_comp = input("Who Has Better SG (team1, team2): ")
+    if sg_comp == "team1":
+        team1_ttl += 1 
+    else:
+        team2_ttl += 1 
+   
+    sf_comp = input("Who Has Better SF (team1, team2): ")
+    if sf_comp == "team1":
+        team1_ttl += 1 
+    else:
+        team2_ttl += 1 
+    
+    pf_comp = input("Who Has Better PF (team1, team2): ")
+    if pf_comp == "team1":
+        team1_ttl += 1 
+    else:
+        team2_ttl += 1 
+
+    c_comp = input("Who Has Better C (team1, team2): ")
+    if c_comp == "team1":
+        team1_ttl += 1 
+    else:
+        team2_ttl += 1 
 
     print("Team1: " + team1_name + " " + str(team1_ttl) + " pts")
     print("Team2: " + team2_name + " " + str(team2_ttl) + " pts")
